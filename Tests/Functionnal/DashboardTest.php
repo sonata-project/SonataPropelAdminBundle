43ded348d863fe7ce0ac3c4de8a21ab42e791503
Kévin Gomez <geek63@gmail.com>
2014-11-07 15:59:34 +0100

    Remove useless assertion

--- Tests/Functionnal/DashboardTest.php
+++ Tests/Functionnal/DashboardTest.php
@@ -10,7 +10,6 @@ class DashboardTest extends WebTestCase
         $crawler = $client->request('GET', '/admin/dashboard');
 
         $this->assertTrue($client->getResponse()->isSuccessful());
-        $this->assertCount(1, $crawler->filter('div.sonata-ba-content:contains("Blog")'), 'There is a "Blog" section');
         $this->assertCount(1, $crawler->filter('div.sonata-ba-content:contains("Posts")'), 'There is a "Posts" sub-section');
     }
 }
